a83c79d1b0f4763d2e78214192c1c25bfde9e056
Aizhee <108914551+Aizhee@users.noreply.github.com>
2024-06-12 14:09:45 +0800

    changed to html

--- connect.php
+++ connect.php
@@ -6,6 +6,9 @@ $pass = "";
 $db = "bakery";
 $conn = new mysqli($host, $user, $pass, $db);
 
+$smtp -> set_charset("utf8");
+
+
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
 }
