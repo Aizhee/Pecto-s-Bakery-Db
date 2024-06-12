166dd011013915e062a67bb86deb891c094d2183
Aizhee <108914551+Aizhee@users.noreply.github.com>
2024-06-12 21:17:26 +0800

    php

--- main.js
+++ main.js
@@ -4,49 +4,49 @@ $(document).ready(function(){
     
     $('#loginBtn').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "LogIn.html");
+        Iframe.attr("src", "LogIn.php");
         $("body").css("overflow", "hidden")
      });
 
      $('#signUpBtn').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "LogIn.html");
+        Iframe.attr("src", "LogIn.php");
         $("body").css("overflow", "hidden")
      });
 
     $('.cart').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "Cart.html");
+        Iframe.attr("src", "Cart.php");
         $("body").css("overflow", "hidden")
      });
 
      $('.fav').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "Favorites.html");
+        Iframe.attr("src", "Favorites.php");
         $("body").css("overflow", "hidden")
      });
 
     $('#ReadMore').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "AboutUs.html");
+        Iframe.attr("src", "AboutUs.php");
         $("body").css("overflow", "hidden")
      });
 
     $('#crackers').on('click', function(){
        Gallery.fadeIn();
-       Iframe.attr("src", "gallery.html?data-filter=BiscuitsAndCrackers");
+       Iframe.attr("src", "gallery.php?data-filter=BiscuitsAndCrackers");
        $("body").css("overflow", "hidden")
     });
 
     $('#bread').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "gallery.html?data-filter=Bread");
+        Iframe.attr("src", "gallery.php?data-filter=Bread");
         $("body").css("overflow", "hidden")
     });
 
     $('#desserts').on('click', function(){
         Gallery.fadeIn();
-        Iframe.attr("src", "gallery.html?data-filter=Desserts");
+        Iframe.attr("src", "gallery.php?data-filter=Desserts");
         $("body").css("overflow", "hidden")
     });
 
@@ -56,31 +56,31 @@ $(document).ready(function(){
     });
 
     $('#Home').on('click', function(){
-        $('body, html').animate({
+        $('body, php').animate({
             scrollTop: $('body').offset().top
         }, 600);
     })
 
     $('#About-us').on('click', function(){
-        $('body, html').animate({
+        $('body, php').animate({
             scrollTop: $('#container-about').offset().top - 160
         }, 600);
     })
 
     $('#Product').on('click', function(){
-        $('body, html').animate({
+        $('body, php').animate({
             scrollTop: $('#products').offset().top - 120
         }, 600);
     })
 
     $('#Gallery').on('click', function(){
-        $('body, html').animate({
+        $('body, php').animate({
             scrollTop: $('#gallery').offset().top - 120
         }, 600);
     })
 
     $('#Contact-us').on('click', function(){
-        $('body, html').animate({
+        $('body, php').animate({
             scrollTop: $('#footer').offset().top - 160
         }, 600);
     })
