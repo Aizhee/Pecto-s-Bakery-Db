1e432da06d7a54f30757e6a5442d7321c7b7202a
Aizhee <108914551+Aizhee@users.noreply.github.com>
2024-06-14 11:34:47 +0800

    refactor: Update add_to_cart.php and add_to_favorites.php to include auto-incremented primary key in INSERT statements

--- add_to_favorites.php
+++ add_to_favorites.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['user_id']) && isset($_POST['product_id'])) {
     $user_id = $_SESSION['user_id'];
     $product_id = $_POST['product_id'];
 
-    $sql = "INSERT INTO likes_table (user_id, product_id) VALUES (?, ?)";
+    $sql = "INSERT INTO likes_table (like_id, user_id, product_id) VALUES (NULL,?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ii", $user_id, $product_id);
 
